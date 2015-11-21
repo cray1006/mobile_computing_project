99736adb92b5e1026378c8895ee15a96eed80c62
Dinh Do <ddo@nd.edu>
2015-11-21 17:04:55 -0500

    changes to the settings view

--- mobile_final_project/mobile_final_project/SettingsViewController.swift
+++ mobile_final_project/mobile_final_project/SettingsViewController.swift
@@ -10,14 +10,30 @@ import Foundation
 
 class SettingsViewController: UITableViewController
 {
+
     @IBOutlet weak var toggle1: UISwitch!
     @IBOutlet weak var toggle2: UISwitch!
-    @IBOutlet weak var toggle3: UISwitch!
+    @IBOutlet weak var rangeslider: UISlider!
+    @IBOutlet weak var rangetext: UILabel!
+    @IBOutlet weak var interest1: UITextField!
+    @IBOutlet weak var interest2: UITextField!
+    @IBOutlet weak var confirmbutton: UIBarButtonItem!
+    @IBOutlet weak var interest3: UITextField!
+    @IBOutlet weak var codename: UITextField!
+    
+
+    @IBAction func rangevaluechanged(sender: UISlider) {
+        let currentvalue = Int(sender.value)
+        rangetext.text = String(currentvalue)
+    }
+
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        rangetext.text = "500"
     }
     
     override func didReceiveMemoryWarning() {
