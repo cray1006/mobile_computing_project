437d4382dedbfdf9dc5a40abd1370da5923bad7b
cray1006 <cray@nd.edu>
2015-12-09 18:17:55 -0500

    increased max range...  again

--- mobile_final_project/mobile_final_project/SettingsViewController.swift
+++ mobile_final_project/mobile_final_project/SettingsViewController.swift
@@ -346,7 +346,7 @@ class SettingsViewController: UITableViewController, CLLocationManagerDelegate
         userRef = Firebase(url: "https://incandescent-torch-8912.firebaseio.com/users")
         
         codename.text = "Anonymous"
-        rangetext.text = "750"
+        rangetext.text = "1000"
         interest1.text = ""
         interest2.text = ""
         interest3.text = ""
